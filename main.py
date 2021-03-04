eeaa9ad4cbc38e026821dc6c77f59797b87e11ca
ihenriquez <ihenriquez86@gmail.com>
2021-03-04 15:56:35 -0300

    First input example.

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+print("the name you entered was", name)
+greeting = "How old are you, "+name+"?"
+Age = input(greeting)
+Year = 2021 - int(Age)
+print(name, ", you were born in:", Year)
+
